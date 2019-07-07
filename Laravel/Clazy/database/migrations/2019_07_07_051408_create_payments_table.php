cb4f6cb87cff3d5db982c13c01ed84eb0e361bc9
huru_26 <w0rk.we6+sns@gmail.com>
2019-07-07 14:06:09 +0800

    （作成）migration file：payments table

--- Laravel/Clazy/database/migrations/2019_07_07_051408_create_payments_table.php
+++ Laravel/Clazy/database/migrations/2019_07_07_051408_create_payments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePaymentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('payment');
             $table->timestamps();
-            $table->integer('salary');
-            $table->date('payroll_date');
+            $table->integer('user_id');
         });
     }
 
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('payments');
     }
 }
