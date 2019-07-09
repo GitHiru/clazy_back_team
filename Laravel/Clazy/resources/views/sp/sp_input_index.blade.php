e5a2312b6e79a14c2b57eb859cd1998db3b9f353
柴田展大 <hi.iniesta111018@gmail.com>
2019-07-09 20:23:38 +0800

    （作成）ララベルにブレイドを作成。

---
+++ Laravel/Clazy/resources/views/sp/sp_input_index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>input</title>
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <link href="../assets/css/sp_input_style.css" rel="stylesheet">
+  <script src="../assets/js/sp_input_style.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+  <!-- アカウント表示 -->
+  <div style="text-align: right; margin-top: 30px; margin-right: 30px;">
+    <a href="">
+      <i class="fas fa-user-circle fa-7x"></i>
+    </a>
+  </div>
+
+  <!-- ロゴ -->
+  <div class="box">
+    <div style="margin-left:90px; margin-top:280px; text-align:center;">
+      <img src="../assets/img/sp_logo.png">
+    </div>
+  <!-- 入力バー -->
+  <form id="form1" action="自分のサイトURL" method="get" style="text-align: center;">
+    <input id="sbox1" id="s" name="s" type="text" style="width: 800px; height: 60px;" placeholder="金額を入力" />
+  </div>
+<!-- 下のフォント達 -->
+<!-- <div align="center" style="margin-top: 770px;">
+  <table border="0">
+    <tr>
+      <td><a href=""><i class="fas fa-home fa-7x"></i></a></td>
+      <td><a class="aaa" href=""><i class="fas fa-external-link-alt fa-7x"></i></a></td>
+      <td><a href=""><i class="fas fa-bars fa-7x"></i></a></td>
+    </tr>
+  </table>
+</div> -->
+
+</body>
+</html>
---
+++ Laravel/Clazy/resources/views/sp/sp_login_index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>login</title>
+</head>
+<body>
+  <div class="box">
+    <div style="margin-left:90px; margin-top:400px; text-align:center;">
+      <img src="../assets/img/sp_logo.png">
+    </div>
+    <hr style="width: 70%; border:0;border-top:3px  solid grey;">
+    <br>
+  <div align="center">
+    <a href="">
+      <img src="../assets/img/login_google.png" alt=""></a>
+      <br>
+    <a href="">
+      <img src="../assets/img/login_facebook.png" alt=""></a>
+  </div>
+  </div>
+</body>
+</html>
