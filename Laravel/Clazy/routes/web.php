41feb7a87a4c01e2e66bd4556eb5ad411071a536
huru_26 <w0rk.we6+sns@gmail.com>
2019-07-10 21:21:28 +0800

    （作成）route：PC loginページの表示　＋　chart機能のルートを作成

--- Laravel/Clazy/routes/web.php
+++ Laravel/Clazy/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () { return view('welcome'); }); //(Laravel)初期データ
+
+Route::get('/', function () { return view('pc.login'); }); //log inページを出力
+
+//Route::get('/', 'ClazyController@chartData')->name('top.index');//chartデータ更新
