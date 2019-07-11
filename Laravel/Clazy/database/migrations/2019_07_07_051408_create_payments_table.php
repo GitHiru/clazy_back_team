b750c7de52855b2bc0e81df2642bf9666776c00b
huru_26 <w0rk.we6+sns@gmail.com>
2019-07-11 15:42:51 +0800

    （修正）migration + seed ：userIDカラムの削除

--- Laravel/Clazy/database/migrations/2019_07_07_051408_create_payments_table.php
+++ Laravel/Clazy/database/migrations/2019_07_07_051408_create_payments_table.php
@@ -17,7 +17,7 @@ class CreatePaymentsTable extends Migration
             $table->bigIncrements('id');
             $table->integer('payment');
             $table->timestamps();
-            $table->integer('user_id');
+            // $table->integer('user_id');
         });
     }
 
