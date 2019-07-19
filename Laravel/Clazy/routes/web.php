428c0713d5f4be2c97fd5eca40730cfa3d192532
huru_26 <w0rk.we6+sns@gmail.com>
2019-07-19 15:54:35 +0800

    （修正）route：　7月19日13時44分マージのルート修正

--- Laravel/Clazy/routes/web.php
+++ Laravel/Clazy/routes/web.php
@@ -20,49 +20,47 @@
 // Route::get('login/{provider}',          'Auth\SocialAccountController@redirectToProvider');
 // Route::get('login/{provider}/callback', 'Auth\SocialAccountController@handleProviderCallback');
 
-
 // ■ ログイン機能
-// Route::group(['middleware' => 'auth'], function() {
-//
-// });
+Route::group(['middleware' => 'auth'], function() {
+    //
+});
 
-// Auth::routes();
+Auth::routes();
+
+
+
+/*******************************************************
+ *     PC + SP 共通
+ *******************************************************/
+// ■ loginページ 表示
+Route::get('/', function () { return view('pc.login0'); });
 
 
 
 /*******************************************************
  *     PC
  *******************************************************/
-// ■ loginページ表示
-// Route::get('/', function () { return view('pc.login'); });
-// ■ firstモーダルページ表示
-Route::get('/modal', function () { return view('pc.modal'); });
-// ■ dashboardページ表示
-// Route::get('/dashboard', function () { return view('pc.dashboard'); });
-
-// ■ ダッシュボードの表示
-Route::get('/', 'ClazyController@firstInformation')->name('Clazy.firstInformation');
+// ■ dashboardページ 表示
+Route::get('/dashboard', 'ClazyController@firstInformation')->name('Clazy.firstInformation');
 // ■ 初期設定モーダルからの投稿
-Route::post('dashboard', 'ClazyController@storeFirst')->name('Clazy.firstInformation'); // 保存処理
-// ■ dashboardページ編集(get)
-Route::get('{id}/dashboard', 'ClazyController@edit')->name('Clazy.edit'); // 編集画面
-// ■ dashboardページ編集(put)
-Route::put('{id}/update', 'ClazyController@update')->name('Clazy.update'); //更新処理
-
-// ■ dashboardページチャート描画
-Route::post('dashboard/chart', 'ClazyController@chart');// （chart）
+Route::post('/dashboard', 'ClazyController@storeFirst')->name('Clazy.firstInformation'); // 保存処理
+// ■ dashboardページ 金額データ編集(get)
+Route::get('/{id}/dashboard', 'ClazyController@edit')->name('Clazy.edit'); // 編集画面
+// ■ dashboardページ 金額データ更新(put)
+Route::put('/{id}/update', 'ClazyController@update')->name('Clazy.update'); //更新処理
+// ■ dashboardページ チャート描画
+Route::post('/dashboard/chart', 'ClazyController@chart');// （chart）
 
 
 
 /*******************************************************
  *    SP
  *******************************************************/
-// ■ (SP)loginページ表示
-Route::get('/sp', function () { return view('sp.login'); });
-// ■ (SP)トップページ表示
-Route::get('sp/top', 'ClazyController@createTop')->name('Clazy.top');
-// ■ (SP)電卓ページ表示
-Route::get('create', 'ClazyController@create')->name('Clazy.create'); // 投稿画面
-Route::post('create', 'ClazyController@store')->name('Clazy.create'); // 保存処理
+// ■ (SP)loginページ 表示
+// Route::get('/sp', function () { return view('sp.login'); });
 
-Auth::routes();
+// ■ (SP)トップページ 表示
+Route::get('/sp/top', 'ClazyController@createTop')->name('Clazy.top');
+// ■ (SP)電卓ページ 表示
+Route::get('/create', 'ClazyController@create')->name('Clazy.create'); // 投稿画面
+Route::post('/create', 'ClazyController@store')->name('Clazy.create'); // 保存処理
